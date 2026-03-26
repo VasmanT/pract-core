bb2862ac5c44341462e19a72635ac90d22807e60
Тихомиров Василий Анатольевич <vasmanvas@gmail.com>
2026-03-26 09:14:21 +0300

    Увидел что гит видит только dbmicro, коммит обновления репа

--- dbmicro/src/main/java/com/example/dbmicro/repository/PlayerRepository.java
+++ dbmicro/src/main/java/com/example/dbmicro/repository/PlayerRepository.java
@@ -15,7 +15,7 @@ public interface PlayerRepository extends JpaRepository<Player, Long> {
         // Формат: 03.12.2025 14:30:45
         var currentDateTime = now(ZoneId.of("Europe/Moscow"))
                 .format(ofPattern("dd.MM.yyyy HH:mm:ss z"));
-        return "крутые данные, - version3(" + currentDateTime + "). Количество игроков в базе: " + count() + ".";
+        return "крутые данные, - version22200(" + currentDateTime + "). Количество игроков в базе: " + count() + ".";
     }
 }
 
