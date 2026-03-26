7d1d50e3e5673f2848ee02838a9b6d30c92269c8
Тихомиров Василий Анатольевич <vasmanvas@gmail.com>
2026-03-26 10:23:05 +0300

    Обновление плеера в dbmicro, он опять стал рекордом

--- dbmicro/src/main/java/com/example/dbmicro/model/Player.java
+++ dbmicro/src/main/java/com/example/dbmicro/model/Player.java
@@ -1,148 +1,94 @@
 package com.example.dbmicro.model;
 
-<<<<<<< HEAD
-//import io.swagger.v3.oas.annotations.media.Schema;
-//import jakarta.persistence.*;
-//import lombok.AllArgsConstructor;
-//import lombok.Getter;
-//import lombok.NoArgsConstructor;
-//import lombok.Setter;
-//
-//import java.time.LocalDate;
-//
-//import static jakarta.persistence.GenerationType.IDENTITY;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
-=======
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
->>>>>>> docker-compose
+import lombok.*;
+//import org.springframework.data.annotation.Id;
+
 import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Table(name = "players")
-<<<<<<< HEAD
-@Schema(description = "Сущность игрока")
-public record Player(
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Schema(description = "Уникальный идентификатор игрока", example = "1")
-        Long id,
-
-        @Column(name = "first_name")
-        @Schema(description = "Имя игрока", example = "Максим")
-        String firstName,
-
-        @Column(name = "last_name")
-        @Schema(description = "Фамилия игрока", example = "Максимов")
-        String lastName,
-
-        @Column(name = "patronymic")
-        @Schema(description = "Отчество игрока", example = "Максимович")
-        String patronymic,
-
-        @Column(name = "gender")
-        @Schema(description = "Пол игрока", example = "man")
-        String gender,
-
-        @Column(name = "game_number")
-        @Schema(description = "Игровой номер игрока", example = "11")
-        byte gameNumber,
-
-        @Column(name = "birth_day")
-        @Schema(description = "Дата рождения игрока", example = "2022-01-27")
-        LocalDate birthDay,
-
-        @Column(name = "city_player")
-        @Schema(description = "Город проживания игрока", example = "Кострома")
-        String cityPlayer
-) {}
-
-
-
-//
-//@Entity
-//@Table(name = "players")
-//@Getter
-//@Setter
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class Player {
-//    @Id
-//    @GeneratedValue(strategy = IDENTITY)
-//    @Schema(description = "Уникальный идентификатор игрока", example = "1")
-//    private Long id;
-//
-//    @Column(name = "first_name")
-//    @Schema(description = "Имя игрока", example = "Максим")
-//    private String firstName;
-//
-//    @Column(name = "last_name")
-//    @Schema(description = "Фамилия игрока", example = "Максимов")
-//    private String lastName;
-//
-//    @Column(name = "patronymic")
-//    @Schema(description = "Отчество игрока", example = "Максимович")
-//    private String patronymic;
-//
-//    @Column(name = "gender")
-//    @Schema(description = "Пол игрока", example = "man")
-//    private String gender;
-//
-//    @Column(name = "game_number")
-//    @Schema(description = "Игровой номер игрока", example = "11")
-//    private byte gameNumber;
-//
-//    @Column(name = "birth_day")
-//    @Schema(description = "Дата рождения игрока", example = "2022, 1, 27")
-//    private LocalDate birthDay;
-//
-//    @Column(name = "city_player")
-//    @Schema(description = "Город проживания игрока", example = "Кострома")
-//    private String cityPlayer;
-//}
-=======
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Schema(description = "Сущность игрока")
 public class Player {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Schema(description = "Уникальный идентификатор игрока", example = "1")
+    private Long id;
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        @Schema(description = "Уникальный идентификатор игрока", example = "1")
-        private Long id;
+    @Column(name = "first_name")
+    @Schema(description = "Имя игрока", example = "Максим")
+    private String firstName;
 
-        @Column(name = "first_name")
-        @Schema(description = "Имя игрока", example = "Максим")
-        private String firstName;
+    @Column(name = "last_name")
+    @Schema(description = "Фамилия игрока", example = "Максимов")
+    private String lastName;
 
-        @Column(name = "last_name")
-        @Schema(description = "Фамилия игрока", example = "Максимов")
-        private String lastName;
+    @Column(name = "patronymic")
+    @Schema(description = "Отчество игрока", example = "Максимович")
+    private String patronymic;
 
-        @Column(name = "patronymic")
-        @Schema(description = "Отчество игрока", example = "Максимович")
-        private String patronymic;
+    @Column(name = "gender")
+    @Schema(description = "Пол игрока", example = "man")
+    private String gender;
 
-        @Column(name = "gender")
-        @Schema(description = "Пол игрока", example = "man")
-        private String gender;
+    @Column(name = "game_number")
+    @Schema(description = "Игровой номер игрока", example = "11")
+    private byte gameNumber;
 
-        @Column(name = "game_number")
-        @Schema(description = "Игровой номер игрока", example = "11")
-        private byte gameNumber;
+    @Column(name = "birth_day")
+    @Schema(description = "Дата рождения игрока", example = "2022, 1, 27")
+    private LocalDate birthDay;
 
-        @Column(name = "birth_day")
-        @Schema(description = "Дата рождения игрока", example = "2022-01-27")
-        private LocalDate birthDay;
-
-        @Column(name = "city_player")
-        @Schema(description = "Город проживания игрока", example = "Кострома")
-        private String cityPlayer;
+    @Column(name = "city_player")
+    @Schema(description = "Город проживания игрока", example = "Кострома")
+    private String cityPlayer;
 }
->>>>>>> docker-compose
+
+//@Entity
+//@Table(name = "players")
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Schema(description = "Сущность игрока")
+//public class Player {
+//
+//        @Id
+//        @GeneratedValue(strategy = GenerationType.IDENTITY)
+//        @Schema(description = "Уникальный идентификатор игрока", example = "1")
+//        private Long id;
+//
+//        @Column(name = "first_name")
+//        @Schema(description = "Имя игрока", example = "Максим")
+//        private String firstName;
+//
+//        @Column(name = "last_name")
+//        @Schema(description = "Фамилия игрока", example = "Максимов")
+//        private String lastName;
+//
+//        @Column(name = "patronymic")
+//        @Schema(description = "Отчество игрока", example = "Максимович")
+//        private String patronymic;
+//
+//        @Column(name = "gender")
+//        @Schema(description = "Пол игрока", example = "man")
+//        private String gender;
+//
+//        @Column(name = "game_number")
+//        @Schema(description = "Игровой номер игрока", example = "11")
+//        private byte gameNumber;
+//
+//        @Column(name = "birth_day")
+//        @Schema(description = "Дата рождения игрока", example = "2022-01-27")
+//        private LocalDate birthDay;
+//
+//        @Column(name = "city_player")
+//        @Schema(description = "Город проживания игрока", example = "Кострома")
+//        private String cityPlayer;
+//}
