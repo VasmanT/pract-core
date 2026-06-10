71c724ad7367f2132c9f7db88d4096bc5022202a
Тихомиров Василий Анатольевич <vasmanvas@gmail.com>
2026-06-10 08:50:55 +0300

    Убрал из общего pom зависимость kafka, так как ее не видят микросервисы. Добавил ее в каждый из сервисов.

--- practice/src/main/java/com/example/practice/service/KafkaProducerService.java
+++ practice/src/main/java/com/example/practice/service/KafkaProducerService.java
@@ -1,7 +1,16 @@
 package com.example.practice.service;
 
+import org.springframework.kafka.core.KafkaTemplate;
+
 public class KafkaProducerService {
     KafkaTemplate<String, String> kafkaTemplate;
 
+   public KafkaProducerService(KafkaTemplate<String, String> kafkaTemplate) {
+       this.kafkaTemplate = kafkaTemplate;
+   }
+
+   public void sendMessage(String topic, String message) {
+       kafkaTemplate.send(topic, message);
+   }
 
 }
